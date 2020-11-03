2bcdd5314b1836d076c48fb211ee09ccbdeb2b90
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-03 09:34:15 +0530

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace LambdaExpressions
+{
+    public class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+        public Person(string ssn,string name,string address, int age)
+        {
+            this.SSN = ssn;
+            this.Name = name;
+            this.Address = address;
+            this.Age = age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to lambda!");
+        }
+    }
+}
